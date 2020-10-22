0c364f0b62fc2e9c7ef4d547ce0b46abb46fad94
seburath <seburath@gmail.com>
2020-10-21 22:40:59 -0500

    Add basic paragraph render

--- lira/ui.py
+++ lira/ui.py
@@ -7,85 +7,86 @@ from prompt_toolkit.layout.containers import HSplit, VSplit, Window
 from prompt_toolkit.layout.layout import Layout
 from prompt_toolkit.styles import Style
 from prompt_toolkit.widgets import SearchToolbar, TextArea, Label
-
-
-from prompt_toolkit.formatted_text import to_formatted_text, HTML, merge_formatted_text, FormattedText
+from prompt_toolkit.formatted_text import to_formatted_text, merge_formatted_text, FormattedText
 from prompt_toolkit import print_formatted_text
 
 
 from lira.book import Book
 from pathlib import Path
 
-path = Path('../tests/data/books/example/intro.rst')
-book = Book(path)
-book.parse()
-print(book)
 
-
-styles =  {
-    'Text' :  '#ff0066 italic',
-    'Strong' :  '#ff0066 italic',
-    'Emphasis' :  '#ff0066 italic',
-    'Literal' :  '#ff0066 italic',
-    'Paragraph' :  '#ff0066 italic',
-    'CodeBlock' :  '#ff0066 italic',
-    'Prompt' :  '#ff0066 italic',
-    'TestBlock' :  '#ff0066 italic',
-    'Section' :  '#ff0066 italic',
+themes = {
+    'default' : {
+        'Text' :  '#fff',
+        'Strong' :  '#fff bold',
+        'Emphasis' :  '#fff italic',
+        'Literal' :  '#fff',
+        'Paragraph' :  '#fff',
+        'CodeBlock' :  '#fff',
+        'Prompt' :  '#fff',
+        'TestBlock' :  '#fff',
+        'Section' :  '#fff',
+        'Separator' :  '#00ff00',
+    }
 }
 
 
-def get_text(content):
-    render = []
-    for node in content.children:
-        if node.is_terminal:
-            text = node.text()
-            style = node.tagname
-            render.append(to_formatted_text(text, styles[style]))
-        else:
-            render += get_text(node)
-    return render
-
-
-render = get_text(book.content[0])
-
-
-tutorial_label = Label(merge_formatted_text(render))
+styles = themes['default']
 
 
 sections = {
     "menu": TextArea(
-        height=40, width=20, style="class:output-field", text="Python-Tutorial\n"
+        height=40, width=25, style=styles['Text'], text="Python-Tutorial\n"
     ),
     "status": TextArea(
-        height=1,
+        height=3,
         prompt=">>> ",
-        style="class:input-field",
+        style=styles['Text'],
         multiline=False,
         wrap_lines=False,
     ),
-    "text": TextArea(height=10, width=40, style="class:output-field", text='text'),
-    "code": TextArea(height=10, width=40, style="class:output-field", text="code"),
-    "prompt": TextArea(height=10, width=40, style="class:prompt", text="prompt"),
-    "vseparator": Window(height=0, width=1, char="|", style="class:line"),
-    "hseparator": Window(height=1, char="-", style="class:line"),
+    "text": TextArea(height=10, width=40, style=styles['Text'], text='text'),
+    "prompt": TextArea(height=10, width=40, style=styles['Text'], text="prompt"),
+    "vseparator": Window(height=0, width=1, char="|", style=styles['Separator']),
+    "hseparator": Window(height=1, char="-", style=styles['Separator']),
 }
 
 
 class TerminalUI:
-    def __init__(self, tutorial_sections):
+    path = Path('../tests/data/books/example/')
+    book = Book(path=path)
+    book.parse()
+
+    chapters = book.chapters[1]
+    chapters.parse()
+
+    kb = KeyBindings()
+
+    @kb.add("c-c")
+    @kb.add("c-q")
+    def _(self, event):
+        " Pressing Ctrl-Q or Ctrl-C will exit the user interface."
+        event.app.exit()
+
+
+    def __init__(self):
         sections_list = []
-        for section in tutorial_sections:
+        for section in ["text", "prompt"]:
             sections_list.append(sections[section])
 
         tutorial = HSplit(sections_list)
+
+        contents  = self.chapters.contents[0]
+        render = self.get_label(contents)
+        label = Label(merge_formatted_text(render))
+
 
         self.container = HSplit([
             VSplit([
                 sections["menu"],
                 sections["vseparator"],
                 HSplit([
-                    tutorial_label,
+                    label,
                     sections["prompt"]
                 ]),
             ]),
@@ -94,35 +95,34 @@ class TerminalUI:
         ])
 
 
-def main():
-    kb = KeyBindings()
+    def get_label(self, contents):
 
-    @kb.add("c-c")
-    @kb.add("c-q")
-    def _(event):
-        " Pressing Ctrl-Q or Ctrl-C will exit the user interface. "
-        event.app.exit()
+        render = []
+        for node in contents.children:
+            if node.is_terminal:
+                text = node.text()
+                style = node.tagname
+                render.append(to_formatted_text(text, styles[style]))
+            else:
+                render += self.get_label(node)
+        render.append(to_formatted_text('\n', ''))
 
-    style = Style(
-        [
-            ("text", "bg:#000000 #ffffff"),
-            ("prompt", "bg:#666666 #ffffff"),
-            ("line", "#004400"),
-        ]
-    )
+        return render
 
-    terminal_ui = TerminalUI(["text", "prompt"])
 
-    application = Application(
-        layout=Layout(terminal_ui.container),
-        key_bindings=kb,
-        style=style,
-        mouse_support=True,
-        full_screen=True,
-    )
+    def run(self):
+        self.app = Application(
+            layout=Layout(self.container),
+            key_bindings=self.kb,
+            mouse_support=True,
+            full_screen=True,
+        )
 
-    application.run()
+        self.app.run()
+
 
 
 if __name__ == "__main__":
-    main()
+    ui = TerminalUI()
+    ui.theme = 'default'
+    ui.run()

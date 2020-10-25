df8470bf9d61b70691ea61a3df0969ea6a50d95b
Santos Gallegos <stsewd@protonmail.com>
2020-10-24 21:41:40 -0500

    LiraApp: load books from config file

--- lira/__main__.py
+++ lira/__main__.py
@@ -2,7 +2,11 @@ from pathlib import Path
 
 from lira.ui import TerminalUI
 
-books_path = Path(__file__).parent / "../tests/data/books/example/"
 
-ui = TerminalUI(books_path)
-ui.run()
+def main():
+    books_path = Path(__file__).parent / "../tests/data/books/example/"
+    ui = TerminalUI(books_path)
+    ui.run()
+
+
+main()
---
+++ lira/books/intro/showcase.rst
@@ -0,0 +1,12 @@
+:level: easy
+:tags: showcase book
+
+Basic elements
+--------------
+
+This is a paragraph with *some* nodes.
+I'm a ``literal`` node, and I'm **strong**.
+
+.. code-block:: python
+
+   print("Hello world!")
